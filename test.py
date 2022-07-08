df579a6c5dcf56fa320ceffd8b68f7cf912781d2
Zachary Tepe <zjtepe@gmail.com>
2022-07-07 21:46:38 -0400

    importing csv

--- test.py
+++ test.py
@@ -5,23 +5,34 @@ import datetime as dt
 import pandas as pd
 import matplotlib as mpl
 
+df = pd.read_csv('archive/annualsnow.csv')
 
-import csv
-with open('archive/annualsnow.csv', 'r') as annualsnow:
-    reader = csv.reader(annualsnow)
-    # for row in reader:
-        # print(annualsnow_data()) # prints annual snow to confirm data is read correctly
+resort = df[(df["Resort"] == 'Alta')]
 
+print(resort)
 
-import csv
-with open('archive/skiresorts.csv', 'r') as resorts:
-    reader = csv.reader(resorts)
-    # for row in reader:
-    #     print(row)
+# import csv
+# with open('archive/annualsnow.csv', 'r') as annualsnow:
+#     reader = csv.reader(annualsnow)
+#     # for row in reader:
+#         # print(annualsnow_data()) # prints annual snow to confirm data is read correctly
 
-import csv
-with open('archive/currentseason.csv', 'r') as archive:
-    reader = csv.reader(archive)
-   # for row in reader:
-      #  print(row)
+# location = pd.read_csv("Location"[annualsnow])
+# for col in reader:
+#     print(annualsnow)
 
+# import csv
+# with open('archive/skiresorts.csv', 'r') as resorts:
+#     reader = csv.reader(resorts)
+#     # for row in reader:
+#     #     print(row)
+
+# import csv
+# with open('archive/currentseason.csv', 'r') as archive:
+#     reader = csv.reader(archive)
+#    # for row in reader:
+#       #  print(row)
+
+# avgbase = "Average Base Depth"(annualsnow)
+#     for col in reader
+#         print(avgbase)
